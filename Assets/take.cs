37850c0900a6a1f480be6f5d3055bb8c8383864f
Maksonjenu <genius690@mail.ru>
2022-07-13 21:48:43 +0700

    initial

---
+++ Assets/take.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class take : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    static Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
+
+    static Interactable interactable;
+
+    
+    void Start()
+    {
+        interactable = this.GetComponent<Interactable>();
+
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(this.gameObject);
+
+        if (startingGrabType != GrabTypes.None)
+        {
+            // Call this to continue receiving HandHoverUpdate messages,
+            // and prevent the hand from hovering over anything else
+            hand.HoverLock(interactable);
+
+            // Attach this object to the hand
+            Debug.Log("tyty ty ty");
+        }
+        else if (isGrabEnding)
+        {
+
+            Debug.Log("QWE QWE QWE");
+            // Detach this object from the hand
+            hand.DetachObject(gameObject);
+
+            // Call this to undo HoverLock
+            hand.HoverUnlock(interactable);
+
+
+            
+
+        }
+    }
+
+        // Update is called once per frame
+    //    void Update()
+    //{
+    //    HandHoverUpdate();
+
+    //}
+}
